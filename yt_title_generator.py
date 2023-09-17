4c4487e196bbdd08f1aa487f20cd3bdf7ef7dc8a
Harkirat Singh Chahal <100717611+iamharkirat@users.noreply.github.com>
2023-09-17 15:04:25 -0700

    Add files via upload

---
+++ yt_title_generator.py
@@ -0,0 +1,44 @@
+import os
+from apikey import apikey
+import streamlit as st
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain, SequentialChain
+from langchain.memory import ConversationBufferMemory
+
+
+os.environ['OPENAI_API_KEY'] = apikey
+
+# App framework
+st.title('⛓️🦜 YouTube GPT Creator')
+prompt = st.text_input('Plug in your prompt here')
+
+# Prompt templates
+title_template = PromptTemplate(
+    input_variables =['topic'],
+    template= 'write me a youtube video title about {topic}'
+)
+
+script_template = PromptTemplate(
+    input_variables =['title'],
+    template= 'write me a youtube video script based on this TITLE: {title}'
+)
+
+# Memory
+memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
+
+# Llms
+llm = OpenAI(temperature=0.9)
+title_chain = LLMChain(llm=llm, prompt=title_template, verbose = True, output_key='title', memory=memory)
+script_chain = LLMChain(llm=llm, prompt=script_template, verbose = True, output_key='script', memory=memory)
+sequential_chain = SequentialChain(chains = [title_chain, script_chain], input_variables=['topic'], output_variables=['title', 'script'], verbose=True)
+
+# Show stuff to the screen if there's a prompt
+if prompt:
+    response = sequential_chain.run({'topic': prompt})
+    st.write(response['title'])
+    st.write(response['script'])
+
+    with st.expander('Message History'):
+        st.info(memory.buffer)
+
